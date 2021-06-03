0adb334509fec606429d2c0dfc4c4ffd6ae7cf8c
gisselP <gissel.piscis@gmail.com>
2021-06-03 09:16:39 -0500

    CSS completado

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+
+ const buttonCipher= document.getElementById('buttonCipher');
+ buttonCipher.addEventListener('click',f1 );
+
+function f1(){
+    let firstText = document.getElementById('firstText');
+    let secondText = document.getElementById('secondText');
+    secondText.innerText=firstText.value;  
+
+}
