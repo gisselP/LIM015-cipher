50eda5ba80802ec39b2d072c5d40eb1559038f17
gisselP <gissel.piscis@gmail.com>
2021-06-05 21:08:02 -0500

    javascript hecho

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,26 @@
 const cipher = {
-  // ...
+    encode:function (text,number){
+        let result="",code;
+
+        for( let i = 0; i<text.length ; i++) {
+            let asciiNum= text[i].charCodeAt();
+            if(asciiNum >= 65 && asciiNum <= 77){
+                code=( ( (asciiNum-65) +number) %26) +65;
+            }else if(asciiNum >= 78 && asciiNum <= 90){
+                code=( ( (asciiNum-65) -number) %26) +65;
+            }else if (asciiNum === 32){
+                code=32  ; 
+            } 
+            result+=String.fromCharCode(code);
+        } 
+        return result;
+      }
+    
+
+
+
 };
+
+
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+ 
+let firstText = document.getElementById('firstText');
+let secondText = document.getElementById('secondText');
+let numberInput = document.getElementById('number');
+let cipherText=" ";
+const buttonCipher= document.getElementById('buttonCipher');
 
 
- const buttonCipher= document.getElementById('buttonCipher');
- buttonCipher.addEventListener('click',f1 );
-
+buttonCipher.addEventListener('click',f1 );
 function f1(){
-    let firstText = document.getElementById('firstText');
-    let secondText = document.getElementById('secondText');
-    secondText.innerText=firstText.value;  
+    let text = firstText.value.toUpperCase();
+    let number= numberInput.value;
+    cipherText=cipher.encode(text,number);
+    secondText.innerText=cipherText;  
 
 }
+
