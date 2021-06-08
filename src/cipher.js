97e80d77e00273f5017780afa82a8a4db360dc50
gisselP <gissel.piscis@gmail.com>
2021-06-08 17:12:56 -0500

    JS mejorado

--- src/cipher.js
+++ src/cipher.js
@@ -1,23 +1,33 @@
 const cipher = {
-    encode:function (text,number){
-        let result="",code;
+    encode:function (number,text){
+        let result="", code;
 
         for( let i = 0; i<text.length ; i++) {
-            let asciiNum= text[i].charCodeAt();
-            if(asciiNum >= 65 && asciiNum <= 77){
-                code=( ( (asciiNum-65) +number) %26) +65;
-            }else if(asciiNum >= 78 && asciiNum <= 90){
-                code=( ( (asciiNum-65) -number) %26) +65;
-            }else if (asciiNum === 32){
-                code=32  ; 
-            } 
+            let asciiNum= text.charCodeAt(i);
+            if(asciiNum >= 65 && asciiNum <= 90){
+                code = ( ( (asciiNum - 65) + number) %26) + 65;
+            }else if(asciiNum===32){
+                code=32;
+            }
             result+=String.fromCharCode(code);
         } 
         return result;
-      }
+    },
     
+    decode:function (number,text){
+        let result="", code;
 
-
+        for( let i = 0; i<text.length ; i++) {
+            let asciiNum= text.charCodeAt(i);
+            if(asciiNum >= 65 && asciiNum <= 90){
+                code = ( ( (asciiNum - 90) - number) %26) + 90;
+            }else if(asciiNum===32){
+                code=32;
+            }
+            result+=String.fromCharCode(code);
+        } 
+        return result;
+    }
 
 };
 
--- src/index.js
+++ src/index.js
@@ -6,15 +6,52 @@ let firstText = document.getElementById('firstText');
 let secondText = document.getElementById('secondText');
 let numberInput = document.getElementById('number');
 let cipherText=" ";
+let decipherText=" ";
 const buttonCipher= document.getElementById('buttonCipher');
+const buttonDecipher= document.getElementById('buttonDecipher');
+const buttonRefresh= document.getElementById('refresh');
 
 
-buttonCipher.addEventListener('click',f1 );
-function f1(){
-    let text = firstText.value.toUpperCase();
-    let number= numberInput.value;
-    cipherText=cipher.encode(text,number);
-    secondText.innerText=cipherText;  
+buttonCipher.addEventListener('click',()=>{
+   secondText.style.display="block";
+   /* alert(allLetter()); */
+   let text = firstText.value.toUpperCase();
+   let number= parseInt(numberInput.value);
+   cipherText= cipher.encode(number,text);
+   secondText.innerText=cipherText;  
+   
 
+});
+
+buttonDecipher.addEventListener('click',()=> {
+   secondText.style.display="block";
+   /* alert(allLetter()); */
+   let text = firstText.value.toUpperCase();
+   let number= parseInt(numberInput.value);
+   decipherText= cipher.decode(number,text);
+   secondText.innerText=decipherText;  
+   
+});
+
+buttonRefresh.addEventListener('click',()=>{
+   secondText.style.display="none";
+   firstText.value="";
+   secondText.value="";
+   numberInput.value="";
+   
+})
+
+   
+   
+
+/* function allLetter(){
+   let text = firstText.value;
+   var letters = /^[A-Za-z]+$/;
+   if(text.match(letters)){
+      return true;
+   }else{
+     alert("solo letras");
+     return false;
+   }
 }
-
+ */
